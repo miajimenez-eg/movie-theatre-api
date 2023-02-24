740c8cd01ca50e5aa36e34f30c5864d5d4bd309f
Mia Jimenez <miajimenez@expediagroup.com>
2023-02-24 12:14:39 +0000

    add endpoints to server file

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const {sequelize} = require('./db');
 const express = require("express");
 const app = express();
 const port = 3000;
+const user = require('./routers/userRouter');
+const show = require('./routers/showRouter');
+
+app.use(express.json());
+app.use('/users', user);
+app.use('/shows', show);
 
 app.listen(port, () => {
     sequelize.sync();
