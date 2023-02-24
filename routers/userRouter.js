e971008338ea0ac5ef8ca0ff759207325e8644dd
Mia Jimenez <miajimenez@expediagroup.com>
2023-02-24 10:48:38 +0000

    requests for user

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,18 +1,38 @@
 const express = require("express");
 const router = express.Router();
 const {User} = require('../models/index');
+const {Show} = require('../models/index');
 const {sequelize} = require('../db');
 const {check, validationResult} = require("express-validator");
 
 // GET all users
-
+router.get('/', async (req, res) => {
+    const allUsers = await User.findAll();
+    res.json(allUsers);
+})
 
 // GET one user
-
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const getUser = await User.findByPk(id);
+    res.json(getUser);
+})
 
 // GET all shows watched by a user (user id in req.params)
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    const getShows = await Show.findAll({ where: { userId: id } });
+    res.json(getShows);
+})
 
 
 // PUT update and add a show if a user has watched it
+router.put('/:id', async (req, res) => {
+    const id = req.params.id;
+    const show = req.body;
+    const user = await User.findByPk(id);
+    await user.addShow(show);
+    res.json(user);
+})
 
 module.exports = router;
